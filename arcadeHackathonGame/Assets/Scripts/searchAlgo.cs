6e0f5f0e4aae3328df27887468c8a93605035d1b
blank-user <cuidavid@ymail.com>
2016-07-12 13:16:38 +1200

    Fixed AdjacencyList

--- arcadeHackathonGame/Assets/Scripts/searchAlgo.cs
+++ arcadeHackathonGame/Assets/Scripts/searchAlgo.cs
@@ -1,129 +1,171 @@
 using System;
 using System.Collections.Generic;
 
-public class Test
-{
+
+class adj{
+
+	// -----------------------------------------------------------------------------------------------
 	int numberOfNodes;
 	int numberOfPokemon;
-	LinkedList<int>[] adjList;
-	const int maxn = 100; //This gives us a sufficiently large list to store stuffs.
 
-	void breadthFirstSearch(int startNode){ // ---------------------------------------------------------
-			bool[] visited = new bool[numberOfNodes]; // records which nodes have already been visited (we do not want to visit a node more than once)
-			int[] distance = new int[numberOfNodes]; // the shortest distance away from the starting node for each of the nodes
-		
-			for(int k = 0; k < numberOfNodes; k++){ // clears any existing garbage data in the arrays + intializes the distance array
-				visited[k] = false;
-				distance[k] = 0;
-			}
-		
-			Queue<int> orderOfVisits = new Queue<int>(); // queue will record which nodes we are going to visit and in which order
-		
-			orderOfVisits.Enqueue(startNode); // 
-			visited[startNode] = true;     // some more initializations
-			distance[startNode] = 0;       //
-		
-			while(orderOfVisits.Count != 0){ // the loop will continue to run until every node has been visited
-				int front = orderOfVisits.Dequeue(); // removes the first element (which is a node index) of the queue and stores in variable
-		
-				foreach (int node in adjList[front]) {
-					if(visited[node] == false){ // if the node hasn't been visited, then update the distance
-						distance[node] = distance[front] + 1; // <- done here
-						orderOfVisits.Enqueue(node); // we then want to check the nodes that are connected to the node we just checked, so add em to the queue.
-		
-						for(int j = 0; j < numberOfPokemon; j++){ // loop through all the pokemon locations and check if any of them match
-							if(node == pokemonNodeIndex[j]){ // if one does match, store the shortest distance to that pokemon in a different location
-								distanceToPokemon[node] = distance[front] + 1; // <- here
-							}
-						}
-					}
-				}
-			}
-		}
+	int[] pokemonNodeIndex;
+	public int[] distance;
 
-	public static void Main()
-	{	// Helper Functions
-		
-		adjList = new LinkedList<int>[maxn]; //Initialise stuff!
+	public LinkedList<int>[] adjList;
 
-		for(int i = 0; i < adjList.Length; i++){ 
+	const int maxn = 100;
+	const int inf = 10000000;
+
+	// -----------------------------------------------------------------------------------------------
+
+	public adj(int numNodes, int numPokemon, int[] pokemonIndex){
+		numberOfNodes = numNodes;
+		numberOfPokemon = numPokemon;
+		pokemonNodeIndex = pokemonIndex;
+		distance = new int[numNodes];
+
+		adjList = new LinkedList<int>[numberOfNodes];
+
+		for(int i = 0; i < adjList.Length; i++){
 			adjList[i] = new LinkedList<int>();
 		}
-		
-		// My graph has 5 nodes (0-4) and 4 edges: (0,1),(0,2),(2,3),(1,2)
-		
-		numberOfNodes = 10;
-		numberOfPokemon = 2;
+	}
 
-		int[] pokemonNodeIndex = new int[numberOfPokemon]; // records where the pokemon are on the graph
-		int[] distanceToPokemon = new int[numberOfPokemon]; // will be filled with the shortest distance to each pokemon
+	public void printGraph(){
+		int i = 0;
 
-		pokemonNodeIndex[0]=3;
-		pokemonNodeIndex[1]=9;
-		
-		adjList = new LinkedList<int>[10]; //Initialise stuff!
-		
-		for(int i = 0; i < adjList.Length; i++){ //Initialise even more stuff!
-			adjList[i] = new LinkedList<int>();
-		}
-		
-		adjList[0].AddFirst(1); //Put your edges in!
-		adjList[0].AddFirst(4);
-		adjList[1].AddFirst(0);
-		adjList[1].AddFirst(2);
-		adjList[1].AddFirst(3);
-		adjList[1].AddFirst(4);
-		adjList[1].AddFirst(5);
-		adjList[2].AddFirst(1);
-		adjList[2].AddFirst(3);
-		adjList[3].AddFirst(1);
-		adjList[3].AddFirst(5);
-		adjList[3].AddFirst(2);
-		adjList[3].AddFirst(6);
-		adjList[4].AddFirst(0);
-		adjList[4].AddFirst(1);
-		adjList[4].AddFirst(5);
-		adjList[4].AddFirst(8);
-		adjList[5].AddFirst(1);
-		adjList[5].AddFirst(3);
-		adjList[5].AddFirst(4);
-		adjList[5].AddFirst(6);
-		adjList[5].AddFirst(7);
-		adjList[5].AddFirst(8);
-		adjList[6].AddFirst(3);
-		adjList[6].AddFirst(5);
-		adjList[6].AddFirst(7);
-		adjList[7].AddFirst(5);
-		adjList[7].AddFirst(6);
-		adjList[7].AddFirst(8);
-		adjList[7].AddFirst(9);
-		adjList[8].AddFirst(4);
-		adjList[8].AddFirst(5);
-		adjList[8].AddFirst(7);
-		adjList[8].AddFirst(9);
-		adjList[9].AddFirst(8);
-		adjList[9].AddFirst(7);
-
-		Console.WriteLine("Test Case Loaded");
-
-		int j = 0;
-
-	    foreach (LinkedList<int> list in adjList)
+		foreach (LinkedList<int> list in adjList)
 	    {
-	        Console.Write("adjList[" + j + "] -> ");
+	        Console.Write("adjList[" + i + "] -> ");
 
 	        foreach (int edge in list)
 	        {
 	            Console.Write(edge + " ");
 	        }
 
-	        ++j;
+	        i++;
 	        Console.WriteLine();
     	}
-    	
-    	//Actually run stuff!
-    	breadthFirstSearch(0);
-    	Console.Write(distanceToPokemon[0] + "," + distanceToPokemon[1]);
-    	
+	}
+
+	public void printPokemonDistance(){
+		for(int i = 0; i < pokemonNodeIndex.Length; i++){
+			Console.Write(distance[pokemonNodeIndex[i]] + " ");
+		}
+
+		Console.WriteLine();
+	}
+
+	public void printDistance(){
+		for(int i = 0; i < distance.Length; i++){
+			Console.Write(distance[i] + " " );
+		}
+
+		Console.WriteLine();
+	}
+
+	public void breadthFirstSearch(int startNode){
+		bool[] visited = new bool[numberOfNodes];
+
+		for(int i = 0; i < numberOfNodes; i++){
+			visited[i] = false;
+			distance[i] = inf;
+		}
+
+		Queue<int> ord = new Queue<int>();
+
+		ord.Enqueue(startNode);
+		visited[startNode] = true;
+		distance[startNode] = 0;
+
+		while(ord.Count != 0){
+			int front = ord.Dequeue();
+
+			foreach(int node in adjList[front]){
+
+				//Console.WriteLine("Debug: " + node);
+
+				if(visited[node] == false){
+					distance[node] = distance[front] + 1;
+					ord.Enqueue(node);
+					visited[node] = true;
+
+					/*for(int i = 0; i < numberOfPokemon; i++){
+						if(node == pokemonNodeIndex[i]){
+							distanceToPokemon[node] = distance[front] + 1;
+						}
+					}*/
+				}
+			}
+		}
+	}
+
+}
+
+public class testGraph{
+
+	public static void Main(){
+
+		int startPosition = 0;
+
+		int nN = 10;
+		int nP = 2;
+		int[] pIndex = new int[2] {3, 9};
+
+		adj testGraph = new adj(nN, nP, pIndex);
+
+		testGraph.adjList[0].AddFirst(1);
+		testGraph.adjList[0].AddFirst(4);
+		testGraph.adjList[1].AddFirst(0);
+		testGraph.adjList[1].AddFirst(2);
+		testGraph.adjList[1].AddFirst(3);
+		testGraph.adjList[1].AddFirst(4);
+		testGraph.adjList[1].AddFirst(5);
+		testGraph.adjList[2].AddFirst(1);
+		testGraph.adjList[2].AddFirst(3);
+		testGraph.adjList[3].AddFirst(1);
+		testGraph.adjList[3].AddFirst(5);
+		testGraph.adjList[3].AddFirst(2);
+		testGraph.adjList[3].AddFirst(6);
+		testGraph.adjList[4].AddFirst(0);
+		testGraph.adjList[4].AddFirst(1);
+		testGraph.adjList[4].AddFirst(5);
+		testGraph.adjList[4].AddFirst(8);
+		testGraph.adjList[5].AddFirst(1);
+		testGraph.adjList[5].AddFirst(3);
+		testGraph.adjList[5].AddFirst(4);
+		testGraph.adjList[5].AddFirst(6);
+		testGraph.adjList[5].AddFirst(7);
+		testGraph.adjList[5].AddFirst(8);
+		testGraph.adjList[6].AddFirst(3);
+		testGraph.adjList[6].AddFirst(5);
+		testGraph.adjList[6].AddFirst(7);
+		testGraph.adjList[7].AddFirst(5);
+		testGraph.adjList[7].AddFirst(6);
+		testGraph.adjList[7].AddFirst(8);
+		testGraph.adjList[7].AddFirst(9);
+		testGraph.adjList[8].AddFirst(4);
+		testGraph.adjList[8].AddFirst(5);
+		testGraph.adjList[8].AddFirst(7);
+		testGraph.adjList[8].AddFirst(9);
+		testGraph.adjList[9].AddFirst(8);
+		testGraph.adjList[9].AddFirst(7);
+
+		Console.WriteLine("Test Cases Loaded.");
+
+
+		Console.WriteLine("Graph Connections: ");
+		testGraph.printGraph();
+		Console.WriteLine("----------------------");
+
+		testGraph.breadthFirstSearch(startPosition);
+		Console.WriteLine("Search Finished Running.");
+
+		Console.WriteLine("Distance to every node: ");
+		testGraph.printDistance();
+
+		Console.WriteLine("Distance to every Pokemon: ");
+		testGraph.printPokemonDistance();
+
 	}
 }
